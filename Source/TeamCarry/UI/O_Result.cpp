f7d5a3cd6790e2127d5e8177d725905985f61f28
Minki Cho <130172235+HermitCho@users.noreply.github.com>
2026-07-20 21:07:58 +0900

    [Fix]: 결과 창 별 출력 테스트

--- Source/TeamCarry/UI/O_Result.cpp
+++ Source/TeamCarry/UI/O_Result.cpp
@@ -1,4 +1,4 @@
-// Fill out your copyright notice in the Description page of Project Settings.
+﻿// Fill out your copyright notice in the Description page of Project Settings.
 
 #include "TeamCarry/UI/O_Result.h"
 #include "CommonButtonBase.h"
